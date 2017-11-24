4ea46097de9b5a971d642ca424b9f189458af017
Abhay2012 <abhay.sehgal2012@gmail.com>
2017-11-24 16:47:19 +0530

    update

--- src/app/pages/home/rank/view/view.rank.component.ts
+++ src/app/pages/home/rank/view/view.rank.component.ts
@@ -90,20 +90,24 @@ export class ViewRankComponent{
 
   validateRank(event:any,index:any,inp:any): any {
     
-    console.log('index ' + this.checkValue.indexOf(inp.value));
-    if((this.checkValue.filter((value)=>{return value == inp.value}).length) == 1 && inp.value >= 1 && inp.value <= this.tableData.length){
+    // console.log('index' + this.checkValue.indexOf(inp.value));
+    if(inp.value == '' || ((this.checkValue.filter((value)=>{return value == inp.value}).length) == 1 && inp.value >= 1 && inp.value <= this.tableData.length)){
       $('#' + inp.id).css({ 'border': '1px solid black' });
     }else{
       $('#' + inp.id).css({ 'border': '1px solid red' });
     }
-    // if (event.srcElement.value > this.tableData.length) {
-    //   $('#' + inp.id).css({ 'border': '1px solid red' });
-    // }
-    // this.checkValue[index]=event.srcElement.value;
-    console.log(this.checkValue);
-    this.isDisable();    
+    for(let i=0;i<this.tableData.length;i++){
+      if(document.getElementById(`rank${i}`).style.borderColor == 'red'){
+        // var element = document.getElementById(`rank${i}`);
+        // console.log(this.checkValue[i]);
+        if((this.checkValue.filter((value)=>{return value == this.checkValue[i]}).length) == 1 && this.checkValue[i] >= 1 && this.checkValue[i] <= this.tableData.length){
+          $('#rank' + i).css({ 'border': '1px solid black' });
+        }else{
+          $('#rank' + i).css({ 'border': '1px solid red' });
+        }  
+      }
+    }
   }
-
 
 
   submitRank() {
